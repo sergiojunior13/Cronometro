009f96ee070e538bbd19537f73259d501ec6ff61
SergioJunior13 <84044055+SergioJunior13@users.noreply.github.com>
2021-06-02 12:08:14 -0300

    Update cronometro.js

--- cronometro.js
+++ cronometro.js
@@ -1,34 +1,19 @@
 let onoff
 let cmin = 0
 let cseg = 0
-let cmil = 0
 let cHora = 0
 let hora = document.getElementById("horas")
 let min = document.getElementById("minutos")
 let seg = document.getElementById("segundos")
-let mili = document.getElementById('milisegundos')
 let startbutton = document.getElementById("start")
 let botoes = document.getElementById("botoes")
 startbutton.addEventListener("click",ativar)
 function ativar() {
     startbutton.classList = "remove"
     clearInterval(onoff)
-    onoff = setInterval(start,10) 
+    onoff = setInterval(start,1000) 
 }
 function start() {
-
-    if (cmil < 100) {
-        if (cmil < 9) {
-            cmil++
-            mili.innerHTML = "0" + cmil
-        } else {
-            cmil++
-            mili.innerHTML = cmil
-        }
-    }
-    if (cmil == 100) {
-        cmil = 0
-        mili.innerHTML = "0" + cmil
         if (cseg < 9) {
             cseg++
             seg.innerHTML = "0" + cseg
@@ -63,7 +48,6 @@ function start() {
             }
         }
     }
-}
 function stop() {
     startbutton.classList = "item"
     clearInterval(onoff)
@@ -71,10 +55,10 @@ function stop() {
 function reset() {
     startbutton.classList = "item"
     clearInterval(onoff)
+    cHora = 0
     cmin = 0
     cseg = 0
-    cmil = 0
+    hora.innerHTML = "00"
     min.innerHTML = "00"
     seg.innerHTML = "00"
-    mili.innerHTML = "00"
 }
