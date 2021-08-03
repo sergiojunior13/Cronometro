13aba4fa6ab00555f1a9111e365dfde701975354
SergioJunior13 <84044055+SergioJunior13@users.noreply.github.com>
2021-08-02 22:42:57 -0300

    Update cronometro.js

--- cronometro.js
+++ cronometro.js
@@ -1,4 +1,4 @@
-let onoff
+let onoff, p = document.getElementsByTagName("p")[0]
 let cmin = 0
 let cseg = 0
 let cHora = 0
@@ -7,47 +7,50 @@ let min = document.getElementById("minutos")
 let seg = document.getElementById("segundos")
 let startbutton = document.getElementById("start")
 let botoes = document.getElementById("botoes")
-startbutton.addEventListener("click",ativar)
+
+startbutton.addEventListener("click", ativar)
+p.addEventListener("click", about)
+
 function ativar() {
     startbutton.classList = "remove"
     clearInterval(onoff)
-    onoff = setInterval(start,1000) 
+    onoff = setInterval(start, 1000)
 }
 function start() {
-        if (cseg < 9) {
-            cseg++
-            seg.innerHTML = "0" + cseg
+    if (cseg < 9) {
+        cseg++
+        seg.innerHTML = "0" + cseg
+    }
+    else {
+        cseg++
+        seg.innerHTML = cseg
+    }
+    if (cseg == 60) {
+        cseg = 0
+        seg.innerHTML = "0" + cseg
+        if (cmin < 9) {
+            cmin++
+            min.innerHTML = `0${cmin}`
         }
         else {
-            cseg++
-            seg.innerHTML = cseg
+            cmin++
+            min.innerHTML = cmin
         }
-        if (cseg == 60) {
-            cseg = 0
-            seg.innerHTML = "0" + cseg
-            if (cmin < 9) {
-                cmin++
-                min.innerHTML = `0${cmin}`
+        if (cmin == 60) {
+            cmin = 0
+            min.innerHTML = "0" + cmin
+            if (cHora < 9) {
+                cHora++
+                horas.innerHTML = `0${cHora}`
+
             }
             else {
-                cmin++
-                min.innerHTML = cmin
-            }
-            if(cmin == 60){
-                cmin = 0
-                min.innerHTML = "0" + cmin
-                if (cHora < 9){
-                    cHora++
-                    horas.innerHTML = `0${cHora}`
-                    
-                }
-                else{
-                    cHora++
-                    horas.innerHTML = cHora
-                }
+                cHora++
+                horas.innerHTML = cHora
             }
         }
     }
+}
 function stop() {
     startbutton.classList = "item"
     clearInterval(onoff)
@@ -61,4 +64,14 @@ function reset() {
     hora.innerHTML = "00"
     min.innerHTML = "00"
     seg.innerHTML = "00"
+}
+
+function about(){
+    let div = document.getElementById("about")
+    if(div.className == ""){
+        div.className = "about"
+    }
+    else {
+        div.className = ""
+    }
 }
